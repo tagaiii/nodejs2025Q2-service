e99c9fd63d5516f3a07fec41cc63e97e15b251be
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-06-16 16:36:44 +0600

    refactor: remove unnecessary import

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,12 +5,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     JwtModule.registerAsync({
-      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET_KEY'),
@@ -21,7 +20,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
     TypeOrmModule.forFeature([User]),
     UsersModule,
-    ConfigModule,
   ],
   providers: [AuthService],
   controllers: [AuthController],
